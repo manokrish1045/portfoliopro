bea729f3fe8f8deb8dd946d38997a02b8cbeb1ec
manokrish2000 <manokrish104525@gmail.com>
2023-09-24 12:34:05 +0530

    correction

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import './Header.css'
-import Content from './Content.jsx'
-import My from '../../assests/myimage.png'
-import HeaderSocialmedia from './HeaderSocialmedia.jsx'
+import React from "react";
+import "./Header.css";
+import Content from "./Content.jsx";
+import My from "../../assests/myimage.png";
+import HeaderSocialmedia from "./HeaderSocialmedia.jsx";
 const Header = () => {
-    return (
-        <header>
-            <div className='container header_container'>
-                <h3>Hi I'm</h3>
-                <h1>Mano</h1>
-                <h3 className='text-light'>FullStack Developer</h3>
-                <Content />
-                <HeaderSocialmedia />
-                <div >
-                    <img className='my' src={My} alt="my "></img>
-                </div>
-                <a href='contact' className='scroll_down'> Scroll Down</a>
+  return (
+    <header>
+      <div className="container header_container">
+        <h3>Hi I'm</h3>
+        <h1>Mano</h1>
+        <h3 className="text-light">FullStack Developer</h3>
+        <Content />
+        <HeaderSocialmedia />
+        <div>
+          <img className="my" src={My} alt="my "></img>
+        </div>
+        <a href="#portfolio" className="scroll_down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+};
 
-            </div>
-        </header>
-
-    )
-}
-
-export default Header
+export default Header;
